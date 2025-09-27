f10f999fd19a91979881130a1fb85c4dd7a29220
DucHuy9725 <truongduchuy9725.com>
2025-09-27 17:27:56 +0700

    Đã hoàn thành giao diện đăng ký

--- miniproject/app/src/main/java/com/example/miniproject/Signup.java
+++ miniproject/app/src/main/java/com/example/miniproject/Signup.java
@@ -1,31 +1,32 @@
 package com.example.miniproject;
 
 import android.os.Bundle;
-import android.content.Intent;
 import android.widget.Button;
+import android.content.Intent;
+import android.widget.ImageButton;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Signup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Button btnSignUp = findViewById(R.id.button2);
-        btnSignUp.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, Signup.class);
+        ImageButton BackSignIn = findViewById(R.id.imageButton);
+        BackSignIn.setOnClickListener(v-> {
+            Intent intent = new Intent(Signup.this, MainActivity.class );
             startActivity(intent);
         });
-
     }
 }
